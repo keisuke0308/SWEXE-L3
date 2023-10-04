c58d9d4facdf53d26d49662c564b99613fa7854b
Ubuntu <ubuntu@ip-172-31-75-125.ec2.internal>
2023-10-04 07:46:19 +0000

    firstcommit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  
+  config.hosts << "c6bad87b103b4cf493ba4cfa41a18f66.vfs.cloud9.us-east-1.amazonaws.com"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
